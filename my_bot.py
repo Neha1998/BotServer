a050c2921396fddc219ba0e1c4cbfb69e2e883fd
Neha <neha.singla@delhivery.com>
2020-05-30 04:04:01 +0530

    my_bot is the main file on which bot runs

---
+++ my_bot.py
@@ -0,0 +1,35 @@
+import discord
+from search_util import g_search
+from DB import cache_search
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hi'):
+            await message.channel.send('hey')
+
+        if message.content.startswith('!google'):
+            query = message.content
+            query = query.replace('!google', '')
+            await message.channel.send(g_search(self, query))
+
+        if message.content.startswith('!recent'):
+            query = message.content
+            query = query.replace('!recent', '')
+            # query to get pattern matching string from table "recents" of the particular user
+            exc = "select distinct searched from recents where user_id='{0}' and searched ~ '.*{1}.*'".format(self.user.id, query)
+            await message.channel.send(cache_search(exc))
+
+
+client = MyClient()
+client.run('NzE1OTUwMzc5NTA1OTQyNjE5.XtErMA.wkx16a5lofwtjgzb3R7vEZyhO5k')
